6e4039c03a19e14f679b882def67bc5f84535800
Aili Huber <alisha.huber@gmail.com>
2021-07-18 11:55:57 -0400

    removed Dotenv

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ require "rails/test_unit/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+# Dotenv::Railtie.load
 
 module June20
   class Application < Rails::Application
